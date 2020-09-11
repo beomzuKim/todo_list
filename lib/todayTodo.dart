87412cbd7396a7b2d9e6ee16db768fe755550c6c
KBZ <kbj3144@yahoo.co.jp>
2020-09-12 02:13:36 +0900

    revise 1

---
+++ lib/todayTodo.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TodayTodo extends StatefulWidget {
+  @override
+  _TodayTodoState createState() => _TodayTodoState();
+}
+
+class _TodayTodoState extends State<TodayTodo> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        elevation: 5,
+        onPressed: (){
+
+        },
+        child: Icon(
+          Icons.add,
+          size: 40,),
+      ),
+      appBar: AppBar(
+      elevation: 0,
+      title: Text('오늘할일', style: TextStyle(color: Colors.white),),
+      centerTitle: true,
+    ),
+    );
+  }
+}
